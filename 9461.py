7edf144e59351448fa41b8a6ce14b851625b15f8
chowh <chowh109@naver.com>
2024-10-05 15:19:06 +0900

    push origin master

--- 9461.py
+++ 9461.py
@@ -1,11 +1,12 @@
-t = int(input())
+P = [[0] for _ in range(101)]
+P[1] = 1
+P[2] = 1
+P[3] = 1
 
-def func(x):    
-    if x == 1 or x == 2 or x == 3:
-        return 1
-    else:
-        return func(x-2) + func(x-3)
+for i in range(4, 101):
+    P[i] = P[i-2] + P[i-3]
     
+t = int(input())
 for i in range(t):
     n = int(input())
-    print(func(n))
+    print(P[n])
