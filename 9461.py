777d38ed6acbec938eb22e1f82f96b21438fcccf
chowh <chowh109@naver.com>
2024-10-06 21:18:37 +0900

    upload 백준 files

--- 9461.py
+++ 9461.py
@@ -10,3 +10,18 @@ t = int(input())
 for i in range(t):
     n = int(input())
     print(P[n])
+
+# import sys
+# input = sys.stdin.readline
+
+# def func(x):    
+#     if x == 1 or x == 2 or x == 3:
+#         return 1
+#     else:
+#         return func(x-2) + func(x-3)
+    
+# t = int(input())
+
+# for i in range(t):
+#     n = int(input())
+#     print(func(n))
