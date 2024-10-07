acdd7860e72ecef6803eb6f0274470acdb1e322d
chowh <chowh109@naver.com>
2024-10-07 15:00:57 +0900

    upload 백준 files

--- 14501.py
+++ 14501.py
@@ -1,7 +1,15 @@
 n = int(input())
-arr = []
+schedule = []
 
 for i in range(n):
-    t, p = map(int,input())
-    arr.append([t,p])
+    t, p = map(int, input().split())
+    schedule.append([t,p])
     
+dp = [0] * (n+1)
+
+for i in range(n):
+    for j in range(i+schedule[i][0], n+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+
+print(dp)
